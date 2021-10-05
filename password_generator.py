e3fe7d0000586597ebdc47cb44dcb7f01d91038d
PRATUL MAURYA <70075276+pratul-maurya@users.noreply.github.com>
2021-10-05 23:01:36 +0530

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,21 @@
+import random
+import string
+
+print('\nWelcome to Password generator!')
+
+length = int(input('\nEnter the length of password: '))
+
+if (length>=10 and length<=25):
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    num = string.digits
+    symbols = string.punctuation
+
+    all = lower + upper + num + symbols
+    temp = random.sample(all,length)
+
+    password = "".join(temp)
+    print(password)
+    
+else:
+    print("Error! Password should between 10 and 25 characters long")
